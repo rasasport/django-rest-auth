4ae8aadfba024b23a62d11dcffbb96b45ea1aca6
RasaSporT <63489341+rasasport@users.noreply.github.com>
2024-06-21 00:27:30 +0300

    Fix

--- rest_auth/serializers.py
+++ rest_auth/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode as uid_decoder
 from django.utils.translation import gettext_lazy as _
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str as force_text
 
 from rest_framework import serializers, exceptions
 from rest_framework.exceptions import ValidationError
